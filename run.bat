a8788115b0e91f3ca0dbc36620fd3d1dc8ff7bec
Blaine Hamilton <btcblaine@gmail.com>
2023-05-23 14:17:50 -0400

    remove redundant .env checks performed in setup

--- run.bat
+++ run.bat
@@ -1,38 +1,4 @@
 @echo off
-
-set "envFile=.env"
-
-REM Check if the .env file exists
-if not exist %envFile% (
-  (
-    echo SPOTIPY_CLIENT_ID=""
-    echo SPOTIPY_CLIENT_SECRET=""
-    echo SPOTIPY_REDIRECT_URI=""
-  ) > %envFile%
-  echo Empty .env file created with required empty values for Spotify API.
-  echo Please fill out the .env file and run the run.bat file again.
-  pause
-  exit /b
-) else (
-  REM Check if the .env file is empty
-  for %%I in (%envFile%) do (
-    if %%~zI equ 0 (
-      (
-        echo SPOTIPY_CLIENT_ID=""
-        echo SPOTIPY_CLIENT_SECRET=""
-        echo SPOTIPY_REDIRECT_URI=""
-      ) > %envFile%
-      echo .env file was empty. Required empty values for Spotify API added.
-      echo Please fill out the .env file and run the run.bat file again.
-      pause
-      exit /b
-    ) else (
-      echo .env file already exists and is not empty.
-    )
-  )
-)
-
-@echo off
 REM Check Python version
 py -3.11 -V >nul 2>&1
 if errorlevel 1 (
@@ -40,14 +6,10 @@ if errorlevel 1 (
   call python -m venv venv
   call venv\Scripts\activate.bat
   call pip install -r requirements.txt -q
-  call python main.py
-  pause
-  exit /b
+  python main.py
 )
 
 call py -3.11 -m venv venv
 call venv\Scripts\activate.bat
 call pip install -r requirements.txt -q
-call python main.py
-
-pause
+python main.py
