264c26304e1f6c75184a78b420ca74c7e777002f
Tw0Brainz <btcblaine@gmail.com>
2023-05-23 21:17:03 -0400

    make regex less prone to error, strip end result, add info on segmentation mode

--- ocr/process.py
+++ ocr/process.py
@@ -6,13 +6,14 @@ def process_image(screenshot):
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     
     # apply OCR on the screenshot
+    # --psm 6 is the page segmentation mode for a single uniform block of text
     text = pytesseract.image_to_string(screenshot,config="--psm 6").lower()
     text = ' '.join(text.split("\n"))
 
-    # regex to match the format '@ song name'
-    pattern = r'@ (.*)'
+    # regex to match the format '@@ song name'
+    pattern = r'@@(.*)'
 
     match = re.search(pattern, text)
     if match and "song name" not in match.group(1).strip():
-        return match.group(1)
+        return match.group(1).strip()
     return None
