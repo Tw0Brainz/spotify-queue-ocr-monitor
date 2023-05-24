769cc44bff89a8b72f2cf75e8bee47392c2c3a17
Tw0Brainz <btcblaine@gmail.com>
2023-05-23 21:17:04 -0400

    add environment variable checks

--- spotify/auth.py
+++ spotify/auth.py
@@ -9,6 +9,17 @@ async def authenticate():
         sp: spotipy.Spotify object
     """
     load_dotenv()
+
+    # Check for existence of environment variables
+    if not os.getenv('SPOTIPY_CLIENT_ID'):
+        raise Exception("Environment variable SPOTIPY_CLIENT_ID is not set")
+
+    if not os.getenv('SPOTIPY_CLIENT_SECRET'):
+        raise Exception("Environment variable SPOTIPY_CLIENT_SECRET is not set")
+
+    if not os.getenv('SPOTIPY_REDIRECT_URI'):
+        raise Exception("Environment variable SPOTIPY_REDIRECT_URI is not set")
+
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
         client_id=os.getenv('SPOTIPY_CLIENT_ID'),
         client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'),
